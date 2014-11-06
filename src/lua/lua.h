626320a7e55fca2218efd26abfcfda4d84842e3b
Rena Kunisaki <hyperhacker@gmail.com>
2014-11-05 19:12:36 -0500

    add rom.header and rom.settings

--- src/lua/lua.h
+++ src/lua/lua.h
@@ -11,4 +11,10 @@
 
 void m64p_lua_load_libs(lua_State *L);
 
+#define LUA_PUSH(L, type, val) lua_push ##type((L), (val))
+#define LUA_SET_FIELD(L, idx, name, type, val) do { \
+	lua_push ##type((L), (val)); \
+	lua_setfield((L), (idx) < 0 ? ((idx)-1) : (idx), (name)); \
+} while(0)
+
 #endif //__M64P_LUA_H__
--- src/lua/m64p_lualib.c
+++ src/lua/m64p_lualib.c
@@ -2,6 +2,14 @@
 #include "lua.h"
 #include "debugger/dbg_memory.h"
 
+enum {
+	ROM_FIELD_HEADER,
+	ROM_FIELD_SETTINGS,
+	NUM_ROM_FIELDS
+};
+static const char *romFieldName[] = {"header", "settings", NULL};
+
+
 static int rom_open(lua_State *L) {
 	const char *path = luaL_checkstring(L, 1);
 	return m64p_lua_return_errcode(L,
@@ -14,9 +22,74 @@ static int rom_close(lua_State *L) {
 }
 
 static int rom_meta_index(lua_State *L) {
-	//XXX
-	printf("rom __index\n");
-	lua_pushnil(L);
+	lua_getfield(L, LUA_REGISTRYINDEX, "rom_fields"); //-1: fields
+	lua_pushvalue(L, 2); //-1: key, -2: fields
+	lua_gettable(L, -2); //-1: val, -2: fields
+	int field = luaL_optinteger(L, -1, -1);
+	lua_pop(L, 2);
+
+	switch(field) {
+		case ROM_FIELD_HEADER: {
+			m64p_rom_header header;
+			if(CoreDoCommand(M64CMD_ROM_GET_HEADER, sizeof(header), &header)) {
+				lua_pushnil(L);
+				break;
+			}
+
+			lua_createtable(L, 0, 16); //-1: tbl
+			LUA_SET_FIELD(L, -1, "init_PI_BSB_DOM1_LAT_REG", integer,
+				header.init_PI_BSB_DOM1_LAT_REG);
+			LUA_SET_FIELD(L, -1, "init_PI_BSB_DOM1_PGS_REG", integer,
+				header.init_PI_BSB_DOM1_PGS_REG);
+			LUA_SET_FIELD(L, -1, "init_PI_BSB_DOM1_PWD_REG", integer,
+				header.init_PI_BSB_DOM1_PWD_REG);
+			LUA_SET_FIELD(L, -1, "init_PI_BSB_DOM1_PGS_REG2", integer,
+				header.init_PI_BSB_DOM1_PGS_REG2);
+			LUA_SET_FIELD(L, -1, "ClockRate", integer, header.ClockRate);
+			LUA_SET_FIELD(L, -1, "PC",        integer, header.PC);
+			LUA_SET_FIELD(L, -1, "Release",   integer, header.Release);
+			LUA_SET_FIELD(L, -1, "CRC1",      integer, header.CRC1);
+			LUA_SET_FIELD(L, -1, "CRC2",      integer, header.CRC2);
+			LUA_SET_FIELD(L, -1, "_x18",      integer, header.Unknown[0]);
+			LUA_SET_FIELD(L, -1, "_x1C",      integer, header.Unknown[1]);
+
+			lua_pushlstring(L, (char*)header.Name, sizeof(header.Name));
+			lua_setfield(L, -2, "Name");
+
+			LUA_SET_FIELD(L, -1, "_x34", integer, header.unknown);
+			LUA_SET_FIELD(L, -1, "Manufacturer_ID", integer,
+				header.Manufacturer_ID);
+			LUA_SET_FIELD(L, -1, "Cartridge_ID", integer, header.Cartridge_ID);
+			LUA_SET_FIELD(L, -1, "Country_code", integer, header.Country_code);
+			break;
+		}
+
+		case ROM_FIELD_SETTINGS: {
+			m64p_rom_settings settings;
+			if(CoreDoCommand(M64CMD_ROM_GET_SETTINGS,
+			  sizeof(settings), &settings)) {
+				lua_pushnil(L);
+				break;
+			}
+
+			lua_createtable(L, 0, 6); //-1: tbl
+			lua_pushlstring(L, settings.goodname, sizeof(settings.goodname));
+			lua_setfield(L, -2, "goodname");
+
+			lua_pushlstring(L, settings.MD5, sizeof(settings.MD5));
+			lua_setfield(L, -2, "MD5");
+
+			LUA_SET_FIELD(L, -1, "savetype", integer, settings.savetype);
+			LUA_SET_FIELD(L, -1, "status",   integer, settings.status);
+			LUA_SET_FIELD(L, -1, "players",  integer, settings.players);
+			LUA_SET_FIELD(L, -1, "rumble",   boolean, settings.rumble);
+			break;
+		}
+
+		default:
+			lua_pushnil(L);
+	}
+
 	return 1;
 }
 
@@ -64,6 +137,15 @@ static int mem_meta_newindex(lua_State *L) {
 
 
 void m64p_lua_load_libs(lua_State *L) {
+	//table of ROM field names
+	lua_createtable(L, 0, NUM_ROM_FIELDS);
+	int i; for(i=0; romFieldName[i]; i++) {
+		lua_pushinteger(L, i);
+		lua_setfield(L, -2, romFieldName[i]);
+	}
+	lua_setfield(L, LUA_REGISTRYINDEX, "rom_fields");
+
+
 	//global m64p table
 	static const luaL_Reg funcs_m64p[] = {
 		{"run",    emu_run},
